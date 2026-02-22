4a6e8dbd30fc8ae38fac0f7809960dc69b47a82c
MissGomezzz <sofiangelagv@gmail.com>
2026-02-21 21:32:28 -0500

    Hago el commit por Paula porque tuvo error con el git :)

---
+++ DOSW-Laboratorio4/DOSW-Laboratorio4/src/main/java/edu/dosw/lab/DOSW_Laboratorio4/DoswLaboratorio4Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.DOSW_Laboratorio4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswLaboratorio4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswLaboratorio4Application.class, args);
+	}
+
+}
